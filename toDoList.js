8631c69011aa7d6f471d5d4e9890a14c2134a64f
kpilszak <klaudia.pilszak@gmail.com>
2018-11-28 16:27:31 +0100

    Rewrite to toggleClass

--- toDoList.js
+++ toDoList.js
@@ -1,16 +1,19 @@
-$("li").click(function(){
-    if($(this).css("color") === "rgba(64, 62, 53, 0.5)"){
-        $(this).css({
-            "color": "rgb(64, 62, 53)",
-            "text-decoration": "none",
-            "font-size": "1.5rem"
-       });
-    }
-    else {
-        $(this).css({
-            "color": "rgba(64, 62, 53, 0.5)",
-            "text-decoration": "line-through",
-            "font-size": "1rem"
-        });
-    }
+$("li").on({
+    click: function(){
+        $(this).toggleClass("doneTask");
+    },
+//    mouseover: function(){
+//        if($(this).css("color") === "rgb(64, 62, 53)"){
+//            $(this).css({
+//                "color": "rgb(239, 57, 78)",
+//                "font-weight": "bold"
+//           });
+//        }
+//        else {
+//            $(this).css({
+//                "color": "rgb(64, 62, 53)",
+//                "font-weight": "normal"
+//            });
+//        }
+//    },
 });
