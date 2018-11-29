ca1cd7df945a2b59d404c37d25f3ff3e17cc6216
kpilszak <klaudia.pilszak@gmail.com>
2018-11-29 14:13:22 +0100

    Add trash icon

--- toDoList.js
+++ toDoList.js
@@ -13,6 +13,6 @@ $("input[type='text']").keypress(function(e){
     if(e.which === 13){
         var taskToDo = $(this).val();
         $(this).val("");
-        $("ul").append("<li><span>X</span>" + taskToDo + "</li>");
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span>" + taskToDo + "</li>");
     }
 });
